25cbbef9277d4c45b980e199c841a4a13817999d
ricardoslv <ricardoslv80@gmail.com>
2021-04-13 22:45:32 +0100

    Add g3 flood broadcast

--- g3 flood broadcast/broadcast.py
+++ g3 flood broadcast/broadcast.py
@@ -37,7 +37,7 @@ def propagateMsg(G):
     msgs_sent = 0
     while(len(received) < len(G.nodes)):
         for r in received.copy():
-            if (r not in received):
+            if (r not in already_Sent):
                 already_Sent.add(r)
                 for n in G.neighbors(r):
                     received.add(n)
